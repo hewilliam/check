67f8490c9747103db4c400058a5bcd5277807b91
William He <whe@users-MacBook-Pro-2.local>
2018-02-07 15:13:30 -0800

    created intial environment

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Check from './check';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <div>checkbook.io</div>
+        <Check/>
+      </div>
+    );
+  }
+}
---
+++ src/components/check.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Check extends Component {
+  render() {
+    return (
+      <div><img src=".src/images/front_check"/></div>
+    );
+  }
+}
